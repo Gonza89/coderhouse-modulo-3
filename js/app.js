47f5843314c723fcd082cf9fced91d38106cb0fc
Gonza89 <g.gonzalo10@hotmail.com>
2016-12-05 18:55:07 -0300

    ng repeat para lista de productos

--- js/app.js
+++ js/app.js
@@ -20,6 +20,24 @@ ng-bind <h1 ng-bind='title'></h1>
     .module("My-app", [])  /*dependencias*/ // declarando una aplicacion
     .controller("ListProductController", function ($scope) {
       $scope.title = "Lista de Productos";
+      $scope.products = [
+     {
+       id: 1,
+       title: 'Computadora',
+       image: 'http://placehold.it/160x200',
+       precio: 5000,
+       featured: true,
+       tags: []
+     },
+     {
+       id: 2,
+       title: 'Computadora nueva',
+       image: 'http://placehold.it/160x200',
+       precio: 10000,
+       featured: false,
+       tags: ['nueva']
+     },
+   ]
     })
     .controller("NavigationController", function ($scope){
       $scope.name = "Gonza";
