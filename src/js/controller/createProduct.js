1d1c6e8c54029aa8e66f0851281f69c95a58d7f0
Gonza89 <g.gonzalo10@hotmail.com>
2016-12-19 02:04:39 -0300

    formulario

--- src/js/controller/createProduct.js
+++ src/js/controller/createProduct.js
@@ -3,14 +3,17 @@ var app = require('../Myapp');
 app
   .controller("createProduct", function($scope) {
     $scope.product = {};
-    $scope.count = 20;
 
     $scope.submit = function() {
+
+      var array = $scope.product.tags.split(",");
+      for (var i=0; i < array.length; i++){
+        array[i] = array[i].trim();
+      }
+
+      $scope.product.tags = array;
       console.log($scope.product);
     };
 
-    $scope.updateCount = function() {
-    
-    }
 
   })
